2bc305de4e1e7c5b82d609cc1635d87e27f4f238
braveadmin <adria_bravo94@collector.us-central1-a.c.empyrean-kit-266717.internal>
2020-04-02 17:52:23 +0000

    added everything

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using collector.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace collector.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(WordModel model)
+        {
+            if (!string.IsNullOrEmpty(model.word))
+            {
+                string contentRootPath = _hostingEnvironment.ContentRootPath;
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(contentRootPath + "/wordsfile/words.txt", true))
+                    {
+                        writer.Write(model.word + ",");
+                    }
+                    ViewBag.ok = true;
+                    model.word = "";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ok = false;
+                }
+            }
+            
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/WordModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace collector.Models
+{
+    public class WordModel
+    {
+        public WordModel()
+        {
+            this.word = "";
+            this.status = false;
+        }
+
+        public string word { get; set; }
+        public bool status { get; set; }
+    }
+}
